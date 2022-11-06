9cca4d925cac7f357ddbf051374fa50a0a58f852
Dmytro Horkun <dmytro.horkun@nure.ua>
2022-11-06 10:01:23 +0200

    - init project styles configuration - add localization module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TranslateModule } from "@ngx-translate/core";
 import { NgxsStoragePluginModule } from "@ngxs/storage-plugin";
 import { NgxsModule } from "@ngxs/store";
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconsRegistrarService } from "./services/icons-registrar.service";
+import { SharedModule } from "./shared/shared.module";
+import { AppInitService } from "./services/app-init.service";
+
+export function initializeAppSteps(appInitService: AppInitService): any {
+  return (): Promise<any> => {
+    return Promise.all([
+      appInitService.init()
+    ]);
+  };
+}
 
 @NgModule({
   declarations: [
@@ -24,9 +36,16 @@ import { AppComponent } from './app.component';
       NgxsStoragePluginModule.forRoot({
         key: [],
       })
-    ]
+    ],
+    SharedModule,
+    TranslateModule
   ],
-  providers: [],
+  providers: [
+    AppInitService,
+    IconsRegistrarService,
+    { provide: APP_INITIALIZER, useFactory: initializeAppSteps, deps: [AppInitService], multi: true },
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
